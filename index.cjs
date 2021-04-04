f5da4cbf0ca3a026e4fe894205355638b3b831d2
e01 <dimitrov.adrian@gmail.com>
2021-04-04 13:22:33 +0300

    Add cjs build script, add github actions

--- index.cjs
+++ index.cjs
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Create new Flake ID generator.
@@ -12,7 +12,7 @@
  *
  * @throws Error
  */
-module.exports = function createFlakeID53({ epoch, workerId }) {
+function createFlakeID53({ epoch, workerId }) {
     let sequenceTime = 0;
     let sequenceRound = 0;
 
@@ -51,7 +51,10 @@ module.exports = function createFlakeID53({ epoch, workerId }) {
      */
     function nextIdPromise(resolve, reject) {
         const current = new Date().valueOf();
-        if (sequenceTime < current) {
+
+        if (current < epoch) {
+            return reject("Epoch is out of range");
+        } else if (sequenceTime < current) {
             sequenceRound = 0;
             sequenceTime = current;
         } else if (sequenceTime > current) {
@@ -64,7 +67,8 @@ module.exports = function createFlakeID53({ epoch, workerId }) {
             setTimeout(nextIdPromise, 1, resolve, reject);
         } else {
             const t = (current - epoch) % 1000000000000;
-            if (t > 900719925473) {
+            const oor = (current - epoch) / 1000000000000;
+            if (oor >= 1 || t > 900719925473) {
                 reject(
                     `Timestamp ${current} is out of range. Reject generating ID as it could exceed Number.MAX_SAFE_INTEGER`
                 );
@@ -81,4 +85,6 @@ module.exports = function createFlakeID53({ epoch, workerId }) {
             workerId: Math.floor((id % 10000) / 1000),
         };
     }
-};
+}
+
+module.exports = createFlakeID53;
